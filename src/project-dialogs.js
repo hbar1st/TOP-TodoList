0c5ddd0f53cad9a3bd22e5217ceb7e1e9f1ec281
hbar1st <35541470+hbar1st@users.noreply.github.com>
2024-11-17 18:54:11 -0500

    refactor project dialog classes

--- src/project-dialogs.js
+++ src/project-dialogs.js
@@ -2,31 +2,35 @@
 import { createProject } from "./project.js";
 export { AddProjectDialog, EditProjectDialog }
 
-
-class AddProjectDialog {
-
-    constructor(docObj, projectList, containerPanel, addingMode, navPanel) {
+class ProjectDialog {
+    constructor(docObj, projectList, containerPanel, navPanel) {
         this.docObj = docObj;
         this.navPanel = navPanel;
         this.projectList = projectList;
         this.containerPanel = containerPanel;
+    }
+
+    show(projectDialog) {
+        // TODO may need to reset the color field and name field if user clicks more than once to add a project
+        projectDialog().showModal();
+    }
+
+}
+class AddProjectDialog extends ProjectDialog {
+
+    constructor(docObj, projectList, containerPanel, addingMode, navPanel) {
+        super(docObj, projectList, containerPanel, navPanel);
         this.projectDialog = this.docObj.querySelector("#add-project-modal");
-        //const headerEl = this.docObj.querySelector("#add-project-modal>h1");
-
-
         const addOrEditBtnParentEl = this.docObj.querySelector("#add-project-modal .button-panel");
-        //<button class="add" type="submit">Add</button>
+
+
         const addBtn = this.docObj.createElement("button");
-
-        // headerEl.textContent = "Add Project";
-
         addBtn.setAttribute("type", "submit");
         addBtn.classList.add("add");
         addBtn.textContent = "Add";
 
         addBtn.addEventListener("click", this.addProject.bind(this));
         addOrEditBtnParentEl.appendChild(addBtn);
-
     }
 
     show() {
@@ -41,10 +45,6 @@ class AddProjectDialog {
 
         const validityState = nameEl.validity;
         if (validityState.valid) {
-            // take the data and tell the project-list object that a new project got added
-            // the project object should inform the storage to update itself
-            // and the project object should inform the nav to update itself
-
             const newProject = createProject(nameEl.value, color);
             this.projectList.add(newProject);
             this.containerPanel.addProject(newProject);
@@ -53,30 +53,21 @@ class AddProjectDialog {
 }
 
 
-class EditProjectDialog {
+class EditProjectDialog extends ProjectDialog {
 
     constructor(docObj, projectList, containerPanel, addingMode, navPanel) {
-        this.docObj = docObj;
-        this.navPanel = navPanel;
-        this.projectList = projectList;
-        this.containerPanel = containerPanel;
+        super(docObj, projectList, containerPanel, navPanel);
+
         this.projectDialog = this.docObj.querySelector("#edit-project-modal");
-        //const headerEl = this.docObj.querySelector("#edit-project-modal>h1");
-
         const addOrEditBtnParentEl = this.docObj.querySelector("#edit-project-modal .button-panel");
-        //<button class="add" type="submit">Add</button>
+
+        this.currentProject = projectList.getProj(containerPanel.getCurrentProjectId());
         const editBtn = this.docObj.createElement("button");
-
-
-        // if not adding, then we must be in edit mode
-        // get the current project in this case
-        this.currentProject = this.projectList.getProj(this.containerPanel.getCurrentProjectId());
 
         const nameEl = this.docObj.querySelector("#edit-project-modal #proj-name");
         const color = this.docObj.querySelector("#edit-project-modal #proj-color");
         nameEl.value = this.currentProject.name;
         color.value = this.currentProject.color;
-        //headerEl.textContent = "Edit Project";
 
         editBtn.setAttribute("type", "submit");
         editBtn.classList.add("save");
@@ -88,18 +79,16 @@ class EditProjectDialog {
 
     show() {
         // TODO may need to reset the color field if user clicks more than once to add a project
-        this.projectDialog.showModal();
+        this.show(this.projectDialog);
     }
 
     editProject(e) {
         console.log("Trying to edit a project: ", e);
+
         const nameEl = this.docObj.querySelector("#edit-project-modal #proj-name");
         const color = this.docObj.querySelector("#edit-project-modal #proj-color");
         const validityState = nameEl.validity;
         if (validityState.valid) {
-            // take the data and tell the project-list object that a new project got added
-            // the project object should inform the storage to update itself
-            // and the project object should inform the nav to update itself
             this.currentProject.name = nameEl.value;
             this.currentProject.color = color.value;
             this.navPanel.displayProjects();
